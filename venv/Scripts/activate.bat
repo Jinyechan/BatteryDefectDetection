36eb86c2e99af9048ad8e9813790e3bd82131aed
uzzin822 <uzzin822@gmail.com>
2025-03-25 10:12:30 +0900

    [front] requirements.txt 추가

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\606-19\Desktop\potato\BatteryDefectDetection\venv
+set VIRTUAL_ENV=C:\Users\606-\BatteryDefectDetection\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
